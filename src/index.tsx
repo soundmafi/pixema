f5acb5507bfb9c55afd5f19342a1e5f002f65a27
soundmafi <soundmafi@gmail.com>
2022-05-07 00:28:39 +0300

    feat(router): add browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 if (!container) {
@@ -10,7 +11,9 @@ if (!container) {
 const root = createRoot(container);
 root.render(
 	<React.StrictMode>
-		{/* <GlobalStyles /> */}
-		<App />
+		<BrowserRouter>
+			{/* <GlobalStyles /> */}
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
