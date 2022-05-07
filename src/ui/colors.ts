776e95a1f1c06688a93ad9032aab848e651a9d79
soundmafi <soundmafi@gmail.com>
2022-05-07 20:05:15 +0300

    feat(ui): add colors

---
+++ src/ui/colors.ts
@@ -0,0 +1,14 @@
+export enum Colors {
+	primary = '#7B61FF',
+	primary2 = '#917CFF',
+	secondary = '#80858B',
+	bgBlack = '#000000',
+	bgDark = '#242426',
+	bgGraphite = '#323537',
+	ctxError = '#FF5154',
+	ctxLigth = '#AFB2B6',
+	ctxWhite = '#FFFFFF',
+	orange = '#F45D2D',
+	yellow = '#F3A608',
+	green = '#00A340',
+}
