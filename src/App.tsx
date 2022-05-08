92eaaced1a2ab430c908f11bb31a3059cb340fb7
soundmafi <soundmafi@gmail.com>
2022-05-08 23:42:38 +0300

    feat(app): add routes and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
+import { routes } from './routes/routes';
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+import Trends from './pages/Trends';
+import MainTemplate from './components/MainTemplate/MainTemplate';
+import { StyledApp } from './Appstyles';
 
-function App() {
-  return (
-    <StyledApp/>
-  );
-}
+const App = () => {
+	return (
+		<StyledApp>
+			<Routes>
+				<Route path={routes.HOME} element={<MainTemplate />}>
+					<Route index element={<Home />} />
+					<Route path={routes.TRENDS} element={<Trends />} />
+					<Route path={routes.FAVORITES} element={<Favorites />} />
+					<Route path={routes.SETTINGS} element={<Settings />} />
+				</Route>
+			</Routes>
+		</StyledApp>
+	);
+};
 
 export default App;
-
-const StyledApp = styled.div`
-  
-`
---
+++ src/Appstyles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+	max-width: 1920px;
+	width: 100%;
+	margin: 0 auto;
+	padding: 40px 62px;
+`;
