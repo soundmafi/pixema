3eeee991ba90d4e221991c739562c99bfc1174cb
soundmafi <soundmafi@gmail.com>
2022-05-09 22:38:25 +0300

    fix(user logged): add cursor pointer

--- src/components/UserLoged/styles.ts
+++ src/components/UserLoged/styles.ts
@@ -6,6 +6,7 @@ export const StyledUserLogged = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	cursor: pointer;
 `;
 export const StyledBadge = styled.div`
 	width: 56px;
