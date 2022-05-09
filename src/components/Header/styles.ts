c8532fe0855b24586fe06553feb93142e5f8cebe
soundmafi <soundmafi@gmail.com>
2022-05-09 22:40:14 +0300

    fix(header styles): add grid area

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.div`
+	grid-area: header;
 	display: grid;
-	grid-template-columns: 0.2fr 1fr 0.2fr;
+	grid-template-columns: 0.15fr 1fr 0.2fr;
 	grid-column-gap: 40px;
 	margin-bottom: 56px;
 `;
