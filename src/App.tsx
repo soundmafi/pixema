fc08a8b7b8254ea67e09e4576a7bbeba9a257ce8
soundmafi <soundmafi@gmail.com>
2022-05-11 01:26:17 +0300

    feat(app): add to app new pages with routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { StyledApp } from './Appstyles';
 import MainTemplate from './components/MainTemplate/MainTemplate';
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import Reset from './pages/Reset';
 import Settings from './pages/Settings';
 import Trends from './pages/Trends';
 import { routes } from './routes/routes';
@@ -19,6 +23,10 @@ const App = () => {
 					<Route path={routes.TRENDS} element={<Trends />} />
 					<Route path={routes.FAVORITES} element={<Favorites />} />
 					<Route path={routes.SETTINGS} element={<Settings />} />
+					<Route path={routes.SIGN_IN} element={<Login />} />
+					<Route path={routes.SIGN_UP} element={<Register />} />
+					<Route path={routes.RESET_PASSWORD} element={<Reset />} />
+					<Route path={routes.NOT_FOUND} element={<NotFound />} />
 				</Route>
 			</Routes>
 		</StyledApp>
