a93593c0b1ae698ce5d3e9485f98235b5bbcc1a0
soundmafi <soundmafi@gmail.com>
2022-05-10 12:12:26 +0300

    refactor(index): update path global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { GlobalStyles } from './GlobalStyles';
+import { GlobalStyles } from './ui/GlobalStyles';
 
 const container = document.getElementById('root');
 if (!container) {
