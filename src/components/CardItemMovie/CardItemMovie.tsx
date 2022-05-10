facfe31585ad7770ec049ee4fe91c9379642138b
soundmafi <soundmafi@gmail.com>
2022-05-10 13:38:02 +0300

    fix(card item movie): move styles to export file

--- src/components/CardItemMovie/CardItemMovie.tsx
+++ src/components/CardItemMovie/CardItemMovie.tsx
@@ -1,7 +1,5 @@
-import styled from 'styled-components';
-import { Colors } from '../../ui/colors';
-import { typography } from '../../ui/typography';
 import MoviePoster from '../MoviePoster/MoviePoster';
+import { StyledCardItemMovie, StyledTitle } from './styles';
 
 interface IMovie {
 	Title: string;
@@ -12,22 +10,10 @@ interface IMovie {
 const CardItemMovie = ({ Poster, Title }: IMovie) => {
 	return (
 		<StyledCardItemMovie>
-			<MoviePoster Poster={Poster}/>
+			<MoviePoster Poster={Poster} />
 			<StyledTitle>{Title}</StyledTitle>
 		</StyledCardItemMovie>
 	);
 };
 
 export default CardItemMovie;
-
-export const StyledCardItemMovie = styled.li`
-	display: block;
-	width: 266px;
-`;
-
-
-
-export const StyledTitle = styled.p`
-	${typography.S2}
-	color: ${Colors.ctxWhite}
-`;
---
+++ src/components/CardItemMovie/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { typography } from "../../ui/typography";
+
+export const StyledCardItemMovie = styled.li`
+	display: block;
+	width: 266px;
+`;
+
+export const StyledTitle = styled.p`
+	${typography.S2}
+`;
