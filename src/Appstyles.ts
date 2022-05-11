45304559b4a15fdeb49c86cf6882734cca3ba09d
soundmafi <soundmafi@gmail.com>
2022-05-11 10:52:02 +0300

    fix(app styles): move styles to main template styles

--- src/Appstyles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledApp = styled.div`
-	max-width: 1920px;
-	width: 100%;
-	margin: 0 auto;
-	padding: 40px 62px;
-`;
--- src/components/MainTemplate/styles.ts
+++ src/components/MainTemplate/styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const StyledMain = styled.div`
+max-width: 1920px;
+	width: 100%;
+	margin: 0 auto;
+	padding: 40px 62px;
 	display: grid;
 	grid-column-gap: 40px;
 	grid-template-columns: 0.15fr 1fr 0.2fr;
