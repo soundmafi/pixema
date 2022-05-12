8c75536c33d22e1d8aacadaa6665f930cd9f3c44
soundmafi <soundmafi@gmail.com>
2022-05-12 23:36:15 +0300

    feat(filter): create component filter

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,11 @@
+import { StyledFilter, StyledTitleFilter } from './styles';
+
+const Filter = () => {
+	return (
+		<StyledFilter>
+			<StyledTitleFilter>Filter</StyledTitleFilter>
+		</StyledFilter>
+	);
+};
+
+export default Filter;
---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Colors } from '../../ui/colors';
+
+export const StyledFilter = styled.div`
+	width: 518px;
+	padding: 48px 40px;
+	background-color: ${Colors.BG_DARK};
+	border-radius: 10px;
+`;
+export const StyledTitleFilter = styled.h2``;
+
+export const StyledTitleParameters = styled.h3``;
+
+export const StyledLine = styled.div`
+	height: 1px;
+	width: 100%;
+	background-color: ${Colors.BG_GRAPHITE};
+`;
