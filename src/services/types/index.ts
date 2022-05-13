201c7074bad71b9e4f4d5fd0a3648cae1bde55c0
soundmafi <soundmafi@gmail.com>
2022-05-13 16:49:04 +0300

    feat(types): add interfaces for mapper

--- src/services/types/index.ts
+++ src/services/types/index.ts
@@ -4,3 +4,33 @@ export interface IRequestParams {
 	type: string;
 	page: number;
 }
+
+export interface IMovieApi {
+	Title: string;
+	imdbID: string;
+	Poster: string;
+	Type: string;
+	Year: string;
+}
+
+export interface IMoviesApiResponse {
+	Response: string;
+	Search: IMovieApi[];
+	TotalResults: string;
+}
+
+export interface IMovie {
+	title: string;
+	imdbID: string;
+	poster: string;
+	type: string;
+	year: string;
+}
+
+export interface IMovies {
+	response: boolean | null;
+	results: IMovie[];
+	totalResults: number;
+	currentPage?: number;
+	totalPages: number;
+}
