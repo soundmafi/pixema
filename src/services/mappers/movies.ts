fe26e0a5dc614a617f514161689b99df97b09886
soundmafi <soundmafi@gmail.com>
2022-05-17 02:03:25 +0300

    fix(mappers): fix destructuring names

--- src/services/mappers/movies.ts
+++ src/services/mappers/movies.ts
@@ -1,17 +1,17 @@
-import { IMovie, IMovieApi, IMovies, IMoviesApiResponse } from '../types';
+import { IMovie, IMovies, IMoviesApiResponse } from '../types';
 
 export const transformMovies = (response: IMoviesApiResponse): IMovies => {
 	return {
 		response: response.Response === 'True' ? true : false,
 		totalResults: Number(response.TotalResults),
 		totalPages: Math.ceil(Number(response.TotalResults) / 10),
-		results: response.Search.map((movie: IMovieApi): IMovie => {
+		results: response.Search.map(({Title, imdbID, Poster,Year, Type}): IMovie => {
 			return {
-				title: movie.Title,
-				imdbID: movie.imdbID,
-				poster: movie.Poster,
-				type: movie.Type,
-				year: movie.Year,
+				title: Title,
+				imdbID: imdbID,
+				poster: Poster,
+				type: Type,
+				year: Year,
 			};
 		}),
 	};
