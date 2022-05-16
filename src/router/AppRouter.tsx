a2f2b11b6c2a29320adb308d10a49a9a6de461dd
soundmafi <soundmafi@gmail.com>
2022-05-17 02:10:59 +0300

    feat(app router): add new rout for movie details page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import MainTemplate from '../components/MainTemplate/MainTemplate';
+import MoviePage from '../components/MoviePage/MoviePage';
 import Favorites from '../pages/Favorites';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import MovieDetails from '../pages/MovieDetails';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
 import Reset from '../pages/Reset';
@@ -11,6 +13,8 @@ import Trends from '../pages/Trends';
 import { routes } from '../routes/routes';
 
 export const AppRouter = () => {
+
+	
 	return (
 		<Routes>
 			<Route path={routes.HOME} element={<MainTemplate />}>
@@ -18,6 +22,7 @@ export const AppRouter = () => {
 				<Route path={routes.TRENDS} element={<Trends />} />
 				<Route path={routes.FAVORITES} element={<Favorites />} />
 				<Route path={routes.SETTINGS} element={<Settings />} />
+				<Route path = {`/:id`} element={<MovieDetails />}/>
 			</Route>
 			<Route path={routes.SIGN_IN} element={<Login />} />
 			<Route path={routes.SIGN_UP} element={<Register />} />
