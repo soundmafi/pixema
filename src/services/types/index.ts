c71d34cdd50e3821cf802c817455a3ec265dbd17
soundmafi <soundmafi@gmail.com>
2022-05-16 12:04:50 +0300

    feat(types): add movie details types

--- src/services/types/index.ts
+++ src/services/types/index.ts
@@ -34,3 +34,58 @@ export interface IMovies {
 	currentPage?: number;
 	totalPages: number;
 }
+
+export interface IMovieDetailsResponse {
+	Title: string;
+	Year: string;
+	Rated: string;
+	Released: string;
+	Runtime: string;
+	Genre: string;
+	Director: string;
+	Writer: string;
+	Actors: string;
+	Plot: string;
+	Language: string;
+	Country: string;
+	Awards: string;
+	Poster: string;
+	Ratings: [];
+	Metascore: string;
+	imdbRating: string;
+	imdbVotes: string;
+	imdbID: string;
+	Type: string;
+	DVD: string;
+	BoxOffice: string;
+	Production: string;
+	Website: string;
+	Response: string;
+}
+
+export interface IMovieDetails {
+	title: string;
+	year: string;
+	released: string;
+	runtime: string;
+	genre: string;
+	director: string;
+	writer: string;
+	actors: string;
+	plot: string;
+	country: string;
+	poster: string;
+	ratings: [];
+	imdbRating: string;
+	imdbID: string;
+	boxOffice: string;
+	production: string;
+	response: boolean;
+	// type: string;
+	// awards: string;
+}
+
+
+export interface IRequestDetailsParams {
+	imdbID: string;
+}
