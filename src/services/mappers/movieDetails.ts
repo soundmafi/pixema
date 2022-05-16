0605892ba55b65c53b11d8d18f1bb926eae4004e
soundmafi <soundmafi@gmail.com>
2022-05-16 12:11:44 +0300

    feat(mappers): add mapper for movie details

---
+++ src/services/mappers/movieDetails.ts
@@ -0,0 +1,25 @@
+import { IMovieDetails, IMovieDetailsResponse } from '../types';
+
+export const transformMovieDetails = (response: IMovieDetailsResponse): IMovieDetails => {
+	return {
+		title: response.Title,
+		year: response.Year,
+		released: response.Released,
+		runtime: response.Runtime,
+		genre: response.Genre,
+		director: response.Director,
+		writer: response.Writer,
+		actors: response.Actors,
+		plot: response.Plot,
+		country: response.Country,
+		poster: response.Poster,
+		ratings: response.Ratings,
+		imdbRating: response.imdbRating,
+		imdbID: response.imdbID,
+		boxOffice: response.BoxOffice,
+		production: response.Production,
+		response: response.Response === 'True' ? true : false,
+		// type: response.Type,
+		// awards: response.Awards,
+	};
+};
