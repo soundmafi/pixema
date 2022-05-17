5e10596b10b7c78b6a15d65631b3a9a35a6a3b56
soundmafi <soundmafi@gmail.com>
2022-05-18 01:09:40 +0300

    feat(authorisation): add condition for display auth users

--- src/components/UserLoged/UserLogged.tsx
+++ src/components/UserLoged/UserLogged.tsx
@@ -1,14 +1,27 @@
 import { StyledBadge, StyledOpenIcon, StyledUserLogged, StyledUsername } from './styles';
 import { ReactComponent as OpenIcon } from './../../assets/Icons/user-open.svg';
+import { StyledButton } from '../SignIn/styles';
+import { useSelector } from 'react-redux';
+import { RootStore } from '../../store/store';
+import { Link } from 'react-router-dom';
+import { routes } from '../../routes/routes';
 
 const UserLogged = () => {
+	const { isAuth, email} = useSelector(({ user }: RootStore) => user);
 	return (
 		<StyledUserLogged>
+			{!isAuth?<>
+				{/* navigate(routes.HOME) */}
+		<Link to={routes.SIGN_IN}><StyledButton>Sign in</StyledButton></Link>
+		<Link to={routes.SIGN_UP}><StyledButton>Sign up</StyledButton></Link>
+		</>
+		:<>
 			<StyledBadge>AL</StyledBadge>
-			<StyledUsername>Alexsei Lapitsky</StyledUsername>
+			<StyledUsername>{email}</StyledUsername>
 			<StyledOpenIcon>
 				<OpenIcon />
 			</StyledOpenIcon>
+			</>}
 		</StyledUserLogged>
 	);
 };
