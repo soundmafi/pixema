548296fe4d710907359838ebbc22c1714e7245d8
soundmafi <soundmafi@gmail.com>
2022-05-19 00:29:00 +0300

    feat(store): add movies & request reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,14 +3,16 @@ import filterStateReducer from './slices/filterStateReducer';
 import userReducer from './slices/userReducer';
 import themeReducer from './slices/themeReducer';
 import favoritesReducer from './slices/favoritesReducer';
-import moviesReducer from './slices/moviesReducer'
+import moviesReducer from './slices/moviesReducer';
+import requestReducer from './slices/requestReducer';
 const store = configureStore({
 	reducer: {
 		user: userReducer,
 		filterState: filterStateReducer,
 		themeType: themeReducer,
 		favorites: favoritesReducer,
-		movies: moviesReducer
+		movies: moviesReducer,
+		requestSearch: requestReducer,
 	},
 });
 export default store;
