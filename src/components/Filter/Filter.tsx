1b771ea73b7ea48714bbf81308d6cf9a205bd4ae
soundmafi <soundmafi@gmail.com>
2022-05-19 17:33:26 +0300

    feat(filter): add search by type/ remove unused elements/ fix styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,45 +1,53 @@
+import { ChangeEvent, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { setStateFilterClose } from '../../store/slices/filterStateReducer';
 import { setRequest } from '../../store/slices/requestReducer';
 import { RootStore } from '../../store/store';
-import { IFilterRequest, IInputData, IOption } from '../../types/types';
+import { IFilterRequest } from '../../types/types';
 import { ButtonCloseIcon } from '../ButtonClose/ButtonCloseIcon';
-import FilterCountrySelect from '../FilterCountrySelect/FilterCountrySelect';
 import FilterInput from '../FilterInput/FilterInput';
-// import { FilterInput } from '../FilterInput/FilterInput';
-import { FilterSorting } from '../FilterSorting/FilterSorting';
-import Input from '../Input/Input';
 import {
 	StyledButtonClose,
 	StyledButtonsContainer,
 	StyledClearFilter,
 	StyledFilter,
 	StyledFilterForm,
+	StyledInput,
+	StyledLabel,
 	StyledLine,
 	StyledShowResult,
+	StyledSortingContainer,
 	StyledTitleFilter,
-	StyledTitleParameters,
 } from './styles';
 
 const Filter = () => {
+	const { register, handleSubmit } = useForm<IFilterRequest>();
 	const dispatch = useDispatch();
 	const { isDisable } = useSelector(({ filterState }: RootStore) => filterState);
-	const { register, handleSubmit } = useForm<IFilterRequest>();
+
+	// state for selected type (Movie/Series/Episode)
+	const [typeMovie, setTypeMovie] = useState<string>('Movie');
+	const isSelected = (value: string): boolean => typeMovie === value;
+	const handleTypeSort = (e: ChangeEvent<HTMLInputElement>): void => {
+		setTypeMovie(e.currentTarget.value);
+	};
+
+	// handle inputs value and write new request
 	const onSubmit: SubmitHandler<IFilterRequest> = ({ title, year }) => {
-		console.log(title, year);
 		const newRequest = {
 			title: title,
 			year: year,
-			// type: type,
-		}
+			type: typeMovie,
+		};
 		dispatch(setRequest(newRequest));
 	};
 
+	//	close filter btn
 	const handleClose = () => {
 		dispatch(setStateFilterClose());
 	};
+
 	return (
 		<>
 			<StyledFilter isDisable={isDisable}>
@@ -47,16 +55,11 @@ const Filter = () => {
 					<ButtonCloseIcon />
 				</StyledButtonClose>
 				<StyledTitleFilter>Filter</StyledTitleFilter>
-				<StyledLine /> 
-				{/* <StyledTitleParameters>Sort by</StyledTitleParameters>
-				<FilterSorting />
-				*/}
-
+				<StyledLine />
 				<StyledFilterForm onSubmit={handleSubmit(onSubmit)}>
-					<StyledTitleParameters>Full or short movie name</StyledTitleParameters>
 					<FilterInput
 						keyData="title"
-						inputName="Movie name"
+						inputName="Full or short movie name"
 						inputType={'text'}
 						placeholder={'Your text'}
 						register={register}
@@ -68,6 +71,43 @@ const Filter = () => {
 						placeholder="Movie year"
 						register={register}
 					/>
+
+					<StyledSortingContainer>
+						<StyledLabel htmlFor="Movie" typeMovie={typeMovie}>
+							Movie
+						</StyledLabel>
+						<StyledInput
+							type="radio"
+							name="sortByMovie"
+							value="Movie"
+							checked={isSelected('Movie')}
+							onChange={handleTypeSort}
+							id="Movie"
+						/>
+						<StyledLabel htmlFor="Series" typeMovie={typeMovie}>
+							Series
+						</StyledLabel>
+						<StyledInput
+							type="radio"
+							name="sortBySeries"
+							value="Series"
+							checked={isSelected('Series')}
+							onChange={handleTypeSort}
+							id="Series"
+						/>
+						<StyledLabel htmlFor="Episode" typeMovie={typeMovie}>
+							Episode
+						</StyledLabel>
+						<StyledInput
+							type="radio"
+							name="sortByEpisode"
+							value="Episode"
+							checked={isSelected('Episode')}
+							onChange={handleTypeSort}
+							id="Episode"
+						/>
+					</StyledSortingContainer>
+
 					<StyledButtonsContainer>
 						<StyledClearFilter>Clear filter</StyledClearFilter>
 						<StyledShowResult>Show results</StyledShowResult>
@@ -80,6 +120,9 @@ const Filter = () => {
 
 export default Filter;
 
+
+
+					{/* <StyledTitleParameters>Full or short movie name</StyledTitleParameters> */}
 {
 	/* <StyledTitleParameters>Genre</StyledTitleParameters>
 			<FilterInput inputName={'Genre'} inputType={'text'} placeholder={'Genre text'} /> */
--- src/components/Filter/styles.ts
+++ src/components/Filter/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Colors } from '../../ui/colors';
+import { typography } from '../../ui/typography';
 
 export const StyledFilter = styled.div<{ isDisable: boolean }>`
 	position: absolute;
@@ -16,7 +17,7 @@ export const StyledFilter = styled.div<{ isDisable: boolean }>`
 	/* max-width: 518px; */
 
 	border-radius: 10px;
-	border: 1px solid ${Colors.CTX_WHITE};
+	border: 1px solid ${Colors.CTX_LIGHT};
 	background-color: ${Colors.BG_DARK};
 
 	box-shadow: 0px 7px 11px 3px rgba(255, 255, 255, 0.48);
@@ -25,7 +26,7 @@ export const StyledFilter = styled.div<{ isDisable: boolean }>`
 export const StyledFilterForm = styled.form``;
 
 export const StyledTitleFilter = styled.h2`
-	margin-bottom: 48px;
+	margin-bottom: 15px;
 	color: ${Colors.CTX_WHITE};
 `;
 
@@ -37,7 +38,7 @@ export const StyledTitleParameters = styled.h3`
 export const StyledLine = styled.div`
 	height: 3px;
 	width: 100%;
-	margin-bottom: 32px;
+	margin-bottom: 40px;
 
 	background-color: ${Colors.BG_GRAPHITE};
 `;
@@ -81,4 +82,41 @@ export const StyledButtonsContainer = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	column-gap: 40px;
+`;
+
+export const StyledSortingContainer = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	column-gap: 5px;
+
+	margin-bottom: 32px;
+
+	overflow: hidden;
+	border-radius: 10px;
+	border: 3px solid ${Colors.BG_GRAPHITE};
+	box-shadow: rgba(255, 255, 255, 0.1) 0px 4px 6px -1px,
+		rgba(255, 255, 255, 0.06) 0px 2px 4px -1px;
+`;
+
+export const StyledTitle = styled.p`
+	${typography.S3}
+`;
+export const StyledInput = styled.input`
+	display: none;
+`;
+
+export const StyledLabel = styled.label<{ htmlFor: string; typeMovie: string }>`
+	padding: 10px 15px;
+
+	${typography.S1}
+	text-align: center;
+	color: ${Colors.CTX_WHITE};
+
+	background-color: ${({ htmlFor, typeMovie }) => {
+		return htmlFor === typeMovie ? `${Colors.ORANGE}` : `${Colors.BG_DARK}`;
+	}};
+
+	transition: 0.3s;
+
+	cursor: pointer;
 `;
