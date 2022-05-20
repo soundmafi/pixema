5dc3696bc22058788409a01e45f8e8902abbe04e
soundmafi <soundmafi@gmail.com>
2022-05-20 15:10:29 +0300

    fix(movies list): fix parametr for transform movies info response

--- src/components/ListMovie/ListMovie.tsx
+++ src/components/ListMovie/ListMovie.tsx
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 import { transformMovies } from '../../services/mappers/movies';
 import { movieApi } from '../../services/movieApi';
 import { setMovies } from '../../store/slices/moviesReducer';
 import { RootStore } from '../../store/store';
 import CardItemMovie from '../CardItemMovie/CardItemMovie';
+import { StyledList } from './styles';
 
 const List = () => {
 	const request = useSelector(({ requestSearch }: RootStore) => requestSearch);
 	const moviesResponse = useSelector(({ movies }: RootStore) => movies);
 	const dispatch = useDispatch();
-	
+
 	useEffect(() => {
-		movieApi.getMoviesByParams(request).then((movies) => dispatch(setMovies(transformMovies(movies))));
+		movieApi
+			.getMoviesByParams(request)
+			.then((movies) => {		
+			dispatch(setMovies(transformMovies(movies,request.page )))});
 	}, [request]);
 
 	return (
@@ -26,20 +29,3 @@ const List = () => {
 };
 
 export default List;
-
-const StyledList = styled.ul`
-	grid-area: outlet;
-
-	display: flex;
-	gap: 40px;
-	flex-wrap: wrap;
-	/* display: grid;
-	grid-template-columns: repeat(auto-fill, minmax (200px, 1fr));
-	grid-gap: 10px; */
-	width: 100%;
-	/* padding: 0;
-	margin: 0; */
-	list-style: none;
-
-	/* max-width: 960px; */
-`;
