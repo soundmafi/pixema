95d94022fb73093541d8ba729eec4ac1240e2b4a
soundmafi <soundmafi@gmail.com>
2022-05-22 23:26:43 +0300

    feat(slider): add slider to recommendation

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -1,24 +1,38 @@
-import styled from 'styled-components';
-import { typography } from '../../ui/typography';
-import List from '../ListMovie/ListMovie';
+import Slider from 'react-slick';
+import { useAppSelector } from '../../store/hooks/useAppSelector';
+import { RootStore } from '../../store/store';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './styles.css';
+import CardRecomendation from '../CardRecomendation/CardRecomendation';
+import { StyledCardContainer, StyledRecommendation, StyledTitleRecommendation } from './styles';
 
 const Recommendations = () => {
+	const sliderSettings = {
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		infinite: true,
+	};
+
+	const moviesList = useAppSelector(({ movies }: RootStore) => movies.results);
+
 	return (
 		<StyledRecommendation>
 			<StyledTitleRecommendation>Recommendation</StyledTitleRecommendation>
-			<List />
+			<Slider {...sliderSettings}>
+				{moviesList.map(({ title, poster, imdbID }) => (
+					<StyledCardContainer key={imdbID}>
+						<CardRecomendation
+							key={imdbID}
+							title={title}
+							poster={poster}
+							imdbID={imdbID}
+						/>
+					</StyledCardContainer>
+				))}
+			</Slider>
 		</StyledRecommendation>
 	);
 };
 
 export default Recommendations;
-
-export const StyledRecommendation = styled.div`
-    grid-area: recommendation;
-`;
-
-export const StyledTitleRecommendation = styled.h2`
-    ${typography.H2}
-    margin-bottom: 40px;
-
-`
