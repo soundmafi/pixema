a18ca7768dae969f33e13ef73d4072dd46d2bf1c
soundmafi <soundmafi@gmail.com>
2022-05-25 17:02:24 +0300

    fix(pagination): fix pagination logic

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from '../../store/hooks/hooks';
 import { getTotalPages } from '../../store/selectors/moviesSelectors';
 import { getRequestSearch } from '../../store/selectors/searchRequestSelectors';
 import { setCurrentPage } from '../../store/slices/requestReducer';
-import { StyledButton, StyledContainer } from './styles';
+import { StyledButton, StyledButtonNav, StyledContainer, StyledTotalPage } from './styles';
 
 const Pagination = () => {
 	const request = useAppSelector(getRequestSearch);
@@ -12,20 +12,21 @@ const Pagination = () => {
 	const totalPages = useAppSelector(getTotalPages);
 
 	const handlePagePrevious = (e: React.MouseEvent<HTMLButtonElement>) => {
-		dispatch(setCurrentPage({ ...request, page: request.page - 1 }));
+		request.page > 1 && 
+			dispatch(setCurrentPage({ ...request, page: request.page - 1 }));
 	};
 
 	const handlePageNext = (e: React.MouseEvent<HTMLButtonElement>) => {
-		dispatch(setCurrentPage({ ...request, page: request.page + 1 }));
+		request.page < totalPages &&
+			dispatch(setCurrentPage({ ...request, page: request.page + 1 }));
 	};
 
 	return (
 		<StyledContainer>
-			<StyledButton onClick={handlePagePrevious}>Previous</StyledButton>
+			<StyledButtonNav onClick={handlePagePrevious}>Previous</StyledButtonNav>
 			<StyledButton>{request.page}</StyledButton>
-			<StyledButton onClick={handlePageNext}>Next</StyledButton>
-
-			{/* <StyledTotalPage>Total {totalPages}</StyledTotalPage> */}
+			<StyledButtonNav onClick={handlePageNext}>Next</StyledButtonNav>
+			<StyledTotalPage>Total pages: {totalPages}</StyledTotalPage>
 		</StyledContainer>
 	);
 };
--- src/components/Pagination/styles.ts
+++ src/components/Pagination/styles.ts
@@ -4,19 +4,28 @@ import { typography } from '../../ui/typography';
 
 export const StyledContainer = styled.div`
 	display: grid;
-	width: 250px;
-	grid-template-columns: repeat(5, 1fr);
+	width: 400px;
+	grid-template-columns: 2fr 0.6fr 2fr 3fr;
 	column-gap: 5px;
 	margin-bottom: 20px;
+	align-items: center;
 `;
 
 export const StyledButton = styled.button`
 	padding: 2px 5px;
-	${typography.S3}
+	${typography.B1}
 	border: 1px solid ${Colors.BG_GRAPHITE};
-	border-radius: 8px;
+	border-radius: 10px;
+`;
+export const StyledButtonNav = styled.button`
+	padding: 2px 5px;
+	${typography.B1}
+	border: 1px solid ${Colors.BG_GRAPHITE};
+	border-radius: 10px;
 `;
 
-// export const StyledTotalPage = styled.p`
+export const StyledTotalPage = styled.p`
+text-align: center;
 
-// `;
+
+`;
