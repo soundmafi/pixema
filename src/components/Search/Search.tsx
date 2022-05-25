9124c2e8108c3ebc39ac59d8b435dd624fb15ef0
soundmafi <soundmafi@gmail.com>
2022-05-25 16:57:15 +0300

    feat(search): fix search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,16 +1,39 @@
-import { ChangeEvent, MouseEvent } from 'react';
+import { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { setStateFilterOpen } from '../../store/slices/filterStateReducer';
 import { StyledButtonOpen, StyledLabel, StyledSearch } from './styles';
 import { FilterOpenIcon } from '../../assets/Icons';
-import { useAppDispatch } from '../../store/hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks/hooks';
+import { movieApi } from '../../services/movieApi';
+import { setMovies } from '../../store/slices/moviesReducer';
+import { transformMovies } from '../../services/mappers/movies';
+import { RootStore } from '../../store/store';
+import { setRequest } from '../../store/slices/requestReducer';
 
 const Search = () => {
 	const dispatch = useAppDispatch();
-	const { register, handleSubmit } = useForm();
+	const { register } = useForm();
+	const request = useAppSelector(({ requestSearch }: RootStore) => requestSearch);
+
 	const handlerSearch = (e: ChangeEvent<HTMLInputElement>) => {
-		return console.log(e.target.value);
+		const newRequest = {
+			title: e.target.value,
+			year: '',
+			type: '',
+			page: 1,
+		};
+
+		const delayRequest = setTimeout(() => {
+			dispatch(setRequest(newRequest));
+		}, 2000);
+		return () => clearTimeout(delayRequest);
 	};
+
+	useEffect(() => {
+		movieApi.getMovieTitleSearch(request).then((movies) => {
+			dispatch(setMovies(transformMovies(movies)));
+		});
+	}, [request]);
 
 	const handleOpen = (e: MouseEvent<HTMLElement>): void => {
 		e.preventDefault();
