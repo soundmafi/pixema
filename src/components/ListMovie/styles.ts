2b612c87c39b6ce4d5a368ee75529213abe6c8cf
soundmafi <soundmafi@gmail.com>
2022-05-26 14:32:49 +0300

    fix(list styles): fix display from flex to grid

--- src/components/ListMovie/styles.ts
+++ src/components/ListMovie/styles.ts
@@ -1,18 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledList = styled.ul`
-	display: flex;
-	gap: 40px;
-	flex-wrap: wrap;
-	/* display: grid;
-	grid-template-columns: repeat(auto-fill, minmax (200px, 1fr));
-	grid-gap: 10px; */
-	width: 100%;
-	/* padding: 0;
-	margin: 0; */
-	list-style: none;
 
-	/* max-width: 960px; */
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(266px, 1fr));
+	grid-gap: 10px; 
 `;
 
 export const ListContainer = styled.div`
