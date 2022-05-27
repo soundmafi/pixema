7f81222cccd48eca01deacbfc2dd0e6faad7bbb1
soundmafi <soundmafi@gmail.com>
2022-05-27 14:08:21 +0300

    fix(mobile menu): add state open/close mobile menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,11 +1,23 @@
+import React, { useState } from 'react';
 import { MenuMobileIcon } from '../../assets/Icons';
+import NavBar from '../NavBar/NavBar';
 import { StyledMenuContainer } from './styles';
 
 const MobileMenu = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		!isOpen ? setIsOpen(true) : setIsOpen(false);
+	};
+
 	return (
-		<StyledMenuContainer>
-			<MenuMobileIcon />
-		</StyledMenuContainer>
+		<>
+			<StyledMenuContainer onClick={handleToggle}>
+				<MenuMobileIcon />
+			</StyledMenuContainer>
+
+			<NavBar isOpen={isOpen} />
+		</>
 	);
 };
 
