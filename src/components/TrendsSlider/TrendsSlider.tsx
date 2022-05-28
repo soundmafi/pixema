72951a852e95842420afbd2366363e2708d7e798
soundmafi <soundmafi@gmail.com>
2022-05-28 23:29:34 +0300

    feat(card slider): create slider for trends

---
+++ src/components/TrendsSlider/TrendsSlider.tsx
@@ -0,0 +1,61 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './../TrendsSlider/styles.css'
+import { StyledCardContainer, StyledRecommendation } from './styles';
+import trendsList from './../../responseMovie.json';
+import CardTrends from '../CardTrends/CardTrends';
+
+const TrendsSlider = () => {
+	let slidersCounting = 4;
+	let slidersScroll = 3;
+
+	const sliderSettings = {
+		slidesToShow: slidersCounting,
+		slidesToScroll: slidersScroll,
+		infinite: true,
+		dots: true,
+		speed: 1000,
+		cssEase: 'linear',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
+
+	return (
+		<StyledRecommendation>
+			<Slider {...sliderSettings}>
+				{trendsList.Search.map(({ title, poster, imdbID }) => (
+					<StyledCardContainer key={imdbID}>
+						<CardTrends key={imdbID} title={title} poster={poster} imdbID={imdbID} />
+					</StyledCardContainer>
+				))}
+			</Slider>
+		</StyledRecommendation>
+	);
+};
+
+export default TrendsSlider;
