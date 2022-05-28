6e5b596d96fba8cab2c06f641940fb1fc02760e5
soundmafi <soundmafi@gmail.com>
2022-05-29 01:38:49 +0300

    feat(reset password): add logic to reset password

--- src/components/ResetPassword/ResetPassword.tsx
+++ src/components/ResetPassword/ResetPassword.tsx
@@ -1,23 +1,44 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { IInputData } from '../../types/types';
 import Input from '../Input/Input';
-import {
-	StyledButton,
-	StyledResetForm,
-	StyledTitle,
-} from './styles';
+import { StyledButton, StyledResetForm, StyledTitle } from './styles';
+
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { routes } from '../../routes/routes';
+
 
 const ResetPassword = () => {
 	const { register, handleSubmit } = useForm<IInputData>();
+	const navigate = useNavigate();
 
 	const onSubmit: SubmitHandler<IInputData> = (data) => {
-		console.log(data.email);
+		const auth = getAuth();
+		sendPasswordResetEmail(auth, data.email)
+			.then(() => {
+				// Password reset email sent!
+				// ..
+				console.log('письмо отправлено');
+				navigate(routes.SIGN_IN);
+				
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// ..
+			});
 	};
 	return (
 		<StyledResetForm onSubmit={handleSubmit(onSubmit)}>
 			<StyledTitle>Reset Password</StyledTitle>
-			<Input inputName="Email" inputType="email" placeholder="Your email" keyData="email" register={register}
-				required/>	
+			<Input
+				inputName="Email"
+				inputType="email"
+				placeholder="Your email"
+				keyData="email"
+				register={register}
+				required
+			/>
 			<StyledButton>Reset</StyledButton>
 		</StyledResetForm>
 	);
