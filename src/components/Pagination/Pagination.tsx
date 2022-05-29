875a07ef9003008d118405ed5fae70406dd91cce
soundmafi <soundmafi@gmail.com>
2022-05-29 03:27:01 +0300

    fix(pagination): try fix logic pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -12,19 +12,25 @@ const Pagination = () => {
 	const totalPages = useAppSelector(getTotalPages);
 
 	const handlePagePrevious = (e: React.MouseEvent<HTMLButtonElement>) => {
-		request.page > 1 && 
-			dispatch(setCurrentPage({ ...request, page: request.page - 1 }));
+		request.page > 1 && dispatch(setCurrentPage({ ...request, page: request.page - 1 }));
+	};
+
+	const handlePage = (e: React.MouseEvent<HTMLElement>) => {
+		return Number(e.currentTarget.textContent);
+		// dispatch(setCurrentPage({ ...request, page: Number(e.currentTarget.textContent) }));
 	};
 
 	const handlePageNext = (e: React.MouseEvent<HTMLButtonElement>) => {
-		request.page < totalPages &&
+		request.page < totalPages - 2 &&
 			dispatch(setCurrentPage({ ...request, page: request.page + 1 }));
 	};
 
 	return (
 		<StyledContainer>
 			<StyledButtonNav onClick={handlePagePrevious}>Previous</StyledButtonNav>
-			<StyledButton>{request.page}</StyledButton>
+			{Array.from({ length: totalPages }).map((_, i) => {
+				return i < 3 ? <StyledButton>{request.page + i}</StyledButton> : '';
+			})}
 			<StyledButtonNav onClick={handlePageNext}>Next</StyledButtonNav>
 			<StyledTotalPage>Total pages: {totalPages}</StyledTotalPage>
 		</StyledContainer>
--- src/components/Pagination/styles.ts
+++ src/components/Pagination/styles.ts
@@ -5,7 +5,7 @@ import { typography } from '../../ui/typography';
 export const StyledContainer = styled.div`
 	display: grid;
 	width: 400px;
-	grid-template-columns: 2fr 0.6fr 2fr 3fr;
+	grid-template-columns: 2fr 0.6fr 0.6fr 0.6fr 2fr 3fr;
 	column-gap: 5px;
 	margin-bottom: 20px;
 	align-items: center;
@@ -25,7 +25,5 @@ export const StyledButtonNav = styled.button`
 `;
 
 export const StyledTotalPage = styled.p`
-text-align: center;
-
-
+	text-align: center;
 `;
