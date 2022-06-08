89fcd1ed3a34d0d5192452ff10a03151855e634e
soundmafi <soundmafi@gmail.com>
2022-06-08 17:14:09 +0300

    fix(recommendation): fix styles

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -9,22 +9,92 @@ import { getMoviesList } from '../../store/selectors/moviesSelectors';
 import useWindowSize from '../../store/hooks/useWindowSize';
 
 const Recommendations = () => {
-	const windowWidth = useWindowSize();
-	let slidersCounting = 0;
-	let slidersScroll = 0;
+	let slidersCounting = 4;
+	let slidersScroll = 3;
 
-	if (windowWidth.width > 768) {
-		slidersCounting = 3;
-		slidersScroll = 3;
-	} else {
-		slidersCounting = 3;
-		slidersScroll = 2;
-	}
 	const sliderSettings = {
 		slidesToShow: slidersCounting,
 		slidesToScroll: slidersScroll,
 		infinite: true,
+		dots: true,
+		speed: 600,
+		cssEase: 'linear',
+		responsive: [
+			{
+				breakpoint: 1700,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+
+			{
+				breakpoint: 1420,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+
+			{
+				breakpoint: 1080,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true,
+				},
+			},
+
+
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 415,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					initialSlide: 2,
+				},
+			},
+		],
 	};
+
+
+	// const windowWidth = useWindowSize();
+	// let slidersCounting = 0;
+	// let slidersScroll = 0;
+
+	// if (windowWidth.width > 768) {
+	// 	slidersCounting = 3;
+	// 	slidersScroll = 3;
+	// } else {
+	// 	slidersCounting = 3;
+	// 	slidersScroll = 2;
+	// }
+	// const sliderSettings = {
+	// 	slidesToShow: slidersCounting,
+	// 	slidesToScroll: slidersScroll,
+	// 	infinite: true,
+	// };
 
 	const moviesList = useAppSelector(getMoviesList);
 
