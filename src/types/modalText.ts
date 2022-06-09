6e54393303e656bbaba5a1654e820dc742a638e1
soundmafi <soundmafi@gmail.com>
2022-06-10 02:23:05 +0300

    feat(enum modal text): create enum with text for modals

---
+++ src/types/modalText.ts
@@ -0,0 +1,6 @@
+export enum ModalText {
+    SUCCES_SIGN_UP = 'You are successfully registered!',
+    SUCCES_SIGN_IN = 'You are successfully signed in.',
+    SUCCES_SIGN_OUT = 'You are successfully signed out.',
+    ERROR_SIGN_UP = 'Email or user already exist'
+}
