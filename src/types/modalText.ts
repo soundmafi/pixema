d5f74ea0418dcf1c18a98ec4b82600a46dd1ef2c
soundmafi <soundmafi@gmail.com>
2022-06-11 01:49:08 +0300

    fix(enum modal text): add more text notificationt

--- src/types/modalText.ts
+++ src/types/modalText.ts
@@ -1,6 +1,11 @@
 export enum ModalText {
-    SUCCES_SIGN_UP = 'You are successfully registered!',
-    SUCCES_SIGN_IN = 'You are successfully signed in.',
-    SUCCES_SIGN_OUT = 'You are successfully signed out.',
-    ERROR_SIGN_UP = 'Email or user already exist'
+	SUCCES_SIGN_UP = 'You are successfully registered!',
+	SUCCES_SIGN_IN = 'You are successfully signed in.',
+	SUCCES_SIGN_OUT = 'You are successfully signed out.',
+    RESET_PASSWORD_POPUP = 'Information with next steps sended to your email',
+	SUCCES_USER_INFORMATION_UPDATED = 'Your personal information was updated',
+	SUCCES_USER_PASSWORD_UPDATED = 'Your password was updated',
+
+	ERROR_SIGN_UP = 'Email or user already exist',
+    ERROR_LOGIN = 'Incorrect login or password',
 }
