78e9b703542d965317ce3d34430317264118ed1a
soundmafi <soundmafi@gmail.com>
2022-06-11 01:14:08 +0300

    fix(settings): make inputs controlled

--- src/components/SettingsForm/SettingsForm.tsx
+++ src/components/SettingsForm/SettingsForm.tsx
@@ -1,7 +1,7 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Path, SubmitHandler, useForm, UseFormRegister } from 'react-hook-form';
 import { useAppDispatch, useAppSelector } from '../../store/hooks/hooks';
 import { getUser } from '../../store/selectors/userSelectors';
-import { IInputData } from '../../types/types';
+import { IFilterRequest, IInputData } from '../../types/types';
 import Input from '../Input/Input';
 import Switch from '../Switcher/Switcher';
 import {
@@ -20,6 +20,7 @@ import { getAuth, onAuthStateChanged, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
 import ModalSuccess from '../Modal/ModalSuccess';
 import { setUser } from '../../store/slices/userReducer';
+import SettingInput from '../SettingInput/SettingInput';
 
 const Settings = () => {
 	const { register, handleSubmit } = useForm<IInputData>();
@@ -27,75 +28,83 @@ const Settings = () => {
 	const auth = getAuth();
 	const dispatch = useAppDispatch();
 
-	const [currentUser, setCurrentUser] = useState(user)
+	const [currentName, setCurrentName] = useState(user.user.displayName);
+	const [currentEmail, setCurrentEmail] = useState(user.user.email);
+
 	const [isDisable, setIsDisable] = useState(false);
 
 	const onSubmit: SubmitHandler<IInputData> = (data) => {
-
-		if (data.name !==""){
-
+		if (data.name !== '') {
 			console.log('да!');
-			
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				// dispatch(setUser({...user, name: data.name}))
-				updateProfile(user, {
-					displayName: data.name,
-				})
-				.then(()=>{
-					setIsDisable(true);
-						setTimeout(() => {
-							setIsDisable(false);
-						}, 2000);
-						dispatch(setUser({user, name: data.name}))
-					console.log('всё выполнилось');
-				}
-				)
-				.catch((error) => {
-					setIsDisable(true);
-						setTimeout(() => {
-							setIsDisable(false);
-						}, 2000);
-					// An error occurred
+
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					// dispatch(setUser({...user, name: data.name}))
+					updateProfile(user, {
+						displayName: data.name,
+					})
+						.then(() => {
+							setIsDisable(true);
+							setTimeout(() => {
+								setIsDisable(false);
+							}, 2000);
+							dispatch(setUser({ user, name: data.name }));
+							console.log('всё выполнилось');
+						})
+						.catch((error) => {
+							setIsDisable(true);
+							setTimeout(() => {
+								setIsDisable(false);
+							}, 2000);
+							// An error occurred
+							// ...
+						});
+				} else {
+					// User is signed out
 					// ...
-				});
-			} else {
-				// User is signed out
-				// ...
-			}
-		});
-	}else{
-		console.log('нет');
-		
-	}
+				}
+			});
+		} else {
+			console.log('нет');
+		}
+	};
+
+	const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setCurrentName(e.target.value);
+	};
+	const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setCurrentEmail(e.target.value);
 	};
 
 	return (
 		<StyledForm onSubmit={handleSubmit(onSubmit)}>
-			{isDisable && <ModalSuccess/>}
+			{isDisable && <ModalSuccess />}
 			<StyledBlockContainer>
 				<StyledTitle>Profile</StyledTitle>
 
 				<StyledContainer>
-					<Input
+					<SettingInput
 						inputName="Name"
 						inputType="text"
 						placeholder="Your Name"
 						keyData="name"
 						register={register}
+						handleValue={handleName}
+						value={currentName}
 					/>
-					<Input
+					<SettingInput
 						inputName="Email"
 						inputType="mail"
 						placeholder="Your email"
 						keyData="email"
 						register={register}
+						handleValue={handleEmail}
+						value={currentEmail}
 					/>
 				</StyledContainer>
 			</StyledBlockContainer>
 
-			<StyledBlockContainer>
-			</StyledBlockContainer>
+			<StyledBlockContainer></StyledBlockContainer>
 
 			{/* <StyledBlockContainer>
 				<StyledTitle>Password</StyledTitle>
