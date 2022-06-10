b06e88243b1cde5bec76c7a936fa634ee532306a
soundmafi <soundmafi@gmail.com>
2022-06-10 20:52:20 +0300

    feat(user bar): fix styles

--- src/components/UserBar/styles.ts
+++ src/components/UserBar/styles.ts
@@ -6,26 +6,34 @@ import { typography } from '../../ui/typography';
 export const StyledNavbar = styled.div<{ isOpen: boolean }>`
 	position: absolute;
 	z-index: 20;
-	right: 30px ;
+	left: 0;
 	top: ${({ isOpen }) => {
-		return isOpen ? `100px` : `-300px`;
+		return isOpen ? `85px` : `-330px`;
 	}};
 
-	width: 250px;
+	display: grid;
+
+	width: 100%;
 	padding: 20px;
 	background-color: ${Colors.BG_DARK};
 
-	border-radius: 10px 0px 0px 10px;
+	border-radius: 10px;
+
+	box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+		rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+		rgba(0, 0, 0, 0.09) 0px -3px 5px;
 
 	transition: top 0.5s ease-in-out;
 `;
 
 export const StyledSignOut = styled.button`
 	padding: 5px 10px;
-	margin-top: 10px;
+	margin-top: 30px;
+
+	width: 100%;
 
 	background-color: ${Colors.PRIMARY};
-	border-radius: 10px;
+	border-radius: 5px;
 	color: ${Colors.CTX_WHITE};
 
 	transition: 0.3s;
@@ -46,9 +54,10 @@ export const StyledTitle = styled.h3`
 `;
 
 export const StyledPhoto = styled.img`
+	margin: 0 auto;
+
 	width: 100px;
 	height: 100px;
-
 
 	${media.TABLET} {
 		width: 208px;
@@ -59,8 +68,4 @@ export const StyledPhoto = styled.img`
 
 	border-radius: 10px;
 	object-fit: cover;
-	
-
 `;
-
-
