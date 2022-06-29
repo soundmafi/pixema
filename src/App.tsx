6e4ae22d3245d5fba1bce65524a7a0639fc42584
soundmafi <soundmafi@gmail.com>
2022-06-29 13:58:56 +0300

    refactor(theme color): move defalut theme code to main template component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useSelector } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
-import { RootStore } from './store/store';
 
 const App = () => {
-	const { themeType } = useSelector(({ themeType }: RootStore) => themeType);
-	document.documentElement.setAttribute('theme', themeType);
 	return (
 		<BrowserRouter>
 			<AppRouter />
--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { Outlet } from 'react-router-dom';
 import useWindowSize, { Size } from '../../store/hooks/useWindowSize';
+import { RootStore } from '../../store/store';
 import Aside from '../Aside/Aside';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import { StyledMain } from './styles';
 
 const MainTemplate = () => {
+	const { themeType } = useSelector(({ themeType }: RootStore) => themeType);
+	document.documentElement.setAttribute('theme', themeType);
 	const size: Size = useWindowSize();
 	return (
 		<StyledMain>
