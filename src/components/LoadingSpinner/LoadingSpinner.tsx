06a71f05382e4a4971cbdd0e84dbac9146959d73
soundmafi <soundmafi@gmail.com>
2022-06-30 13:45:04 +0300

    fix(spinner styles): fix spinner for loading styles

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,17 @@
 import { SpinnerCircularSplit } from 'spinners-react';
+import { Container } from './styles';
 
 const LoadingSpinner = () => {
 	return (
-		<SpinnerCircularSplit
-			size={89}
-			thickness={82}
-			speed={65}
-			color="rgba(169, 183, 255, 1)"
-			secondaryColor="rgba(117, 57, 172, 0.16)"
-		/>
+		<Container>
+			<SpinnerCircularSplit
+				size={89}
+				thickness={82}
+				speed={65}
+				color="rgba(169, 183, 255, 1)"
+				secondaryColor="rgba(117, 57, 172, 0.16)"
+			/>
+		</Container>
 	);
 };
 
